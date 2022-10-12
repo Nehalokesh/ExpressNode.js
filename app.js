1ef40e2f5a1a20674171c3f322218eba3baa571f
Nehalokesh <nehal19697@gmail.com>
2022-10-12 18:45:29 +0530

    Task3 Commit

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // Alows the request to continue to the next middleware in line 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><label>Product:</label><input type="text" name="title"><label>Size:</label><input type="text" name="title"><button type="Submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello to Node js');
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
+    res.send('<h1>Hello to Node js</h1>');
 });
-// const server=http.createServer(app);
 
-// server.listen(4000);
 
-app.listen(4000);
+app.listen(4000); 
