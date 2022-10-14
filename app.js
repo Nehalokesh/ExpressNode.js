712c22c804b876959dcf7b2a2d143883cc1cc5be
Nehalokesh <nehal19697@gmail.com>
2022-10-14 16:51:22 +0530

    Task6 Commit

--- app.js
+++ app.js
@@ -1,54 +1,25 @@
-const express = require ('express');
-const chatapp = express();
-const fs = require('fs');
-const bodyParser = require("body-parser")
+const path = require('path');
 
-chatapp.use(bodyParser.urlencoded());
+const express = require('express');
 
+const bodyParser = require('body-parser');
 
-chatapp.get('/' , (req,res)=>{
-    fs.readFile('username.txt',(err,data)=>{
-      if(err)
-      {
-        console.log(err);
-        data ='no chats';
-      }
-      res.send(
-        `${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"
-        >
-        <input type="text" name="message" id="message" placeholder="message">
-        <input type="hidden" name ="username" id="username" >
-        <br>
-        <button type="submit">add</button></form>`);
-    })
+const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+const contactRouter = require('./routes/contact');
+const successRouter = require('./routes/success');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRouter);
+app.use('/contact',contactRouter);
+app.use('/success',successRouter);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
-chatapp.post("/",(req,res)=>
-{
-  if(req.body.message == '')
-  {
-    skip;
-  }
-  fs.writeFile("username.txt", `${req.body.username}:${req.body.message}`,{flag :'a' },(err)=>
-       err ? console.log(err) : res.redirect("/")
-    )
-    console.log(req.body.username);
-    console.log(req.body.message);
-
-})
-
-
-chatapp.get('/login' , (req,res)=>{
-    //console.log('userdetails');
-    res.send(
-      `<form onSubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/" method="get">
-      <input id="username" type="text" name"username" placeholder="username">
-     <br>
-      <button type="submit">Login</button>
- 
-  </form>`);
-
-})
-
-chatapp.listen(5000);
+app.listen(5000);
